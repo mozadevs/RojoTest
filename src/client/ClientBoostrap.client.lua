e56743acb578458e8598f4544814c6652682bbe4
Hamza <contactdrizz@proton.me>
2026-05-05 17:23:53 +0300

    setup the framework

---
+++ src/client/ClientBoostrap.client.lua
@@ -0,0 +1,12 @@
+-- Bootstraps the client: loads Components, then runs Core + UI Controllers through init/start lifecycle.
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Modules = ReplicatedStorage.Modules
+local Loader = require(Modules.Utilities.Loader)
+
+Loader.loadComponents({ Modules.Components })
+
+local Controllers = Loader.load({ Modules.Core, Modules.UI })
+Loader.start(Controllers)
+
+print("[Client] Bootstrap complete")
